7b76129a8f5ea42391b34f10f917f945d519573c
ginkosoul <ginkosoul@gmail.com>
2023-05-21 12:48:34 +0300

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import user from './user.json';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import { App } from 'components/App';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const home = <div className="profile">
+<div className="description">
+  <img
+    src={user.avatar}
+    alt="User avatar"
+    className="avatar"
+  />
+  <p className="name">{user.username}</p>
+  <p className="tag">{user.tag}</p>
+  <p className="location">{user.location}</p>
+</div>
+
+<ul className="stats">
+  <li>
+    <span className="label">Followers</span>
+    <span className="quantity">{user.stats.followers}</span>
+  </li>
+  <li>
+    <span className="label">Views</span>
+    <span className="quantity">{user.stats.views}</span>
+  </li>
+  <li>
+    <span className="label">Likes</span>
+    <span className="quantity">{user.stats.likes}</span>
+  </li>
+</ul>
+</div>
+// const div = React.createElement('div',{
+//   name:'main',
+//   id: 'test-id',
+//   className:'test-class',
+//   children: ['Hello', ' React', ' from', ' UA']
+// })
+
+// console.log(div);
+ReactDOM.createRoot(document.getElementById('root')).render(home)
